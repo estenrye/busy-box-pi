2fcbbf979dc02f86c48bfa68835ba1b0e89e0c85
Esten Rye <esten@ryezone.net>
2018-10-02 04:24:35 -0500

    Write C# Wire implementation.

--- projects/busy-box-pi-gui/Adafruit_PN532/Wire.cs
+++ projects/busy-box-pi-gui/Adafruit_PN532/Wire.cs
@@ -15,51 +15,88 @@ namespace Adafruit_PN532
         private readonly SpiDevice spi;
         private readonly I2cDevice i2c;
 
-        public Wire(int deviceIndex, int slaveAddress, I2cBusSpeed busSpeed = I2cBusSpeed.StandardMode, I2cSharingMode sharingMode = I2cSharingMode.Shared)
+        public Wire(ConnectionType connectionType, int deviceIndex, int address)
         {
-            var i2cSelectorQuery = I2cDevice.GetDeviceSelector();
-            IReadOnlyList<DeviceInformation> devices = DeviceInformation.FindAllAsync(i2cSelectorQuery).GetResults();
+            this.connectionType = connectionType;
+
+            var selectorQuery = string.Empty;
+
+            switch (this.connectionType)
+            {
+                case ConnectionType.I2C:
+                    selectorQuery = I2cDevice.GetDeviceSelector();
+                    break;
+                case ConnectionType.SPI:
+                    selectorQuery = SpiDevice.GetDeviceSelector();
+                    break;
+            }
+
+            IReadOnlyList<DeviceInformation> devices = DeviceInformation.FindAllAsync(selectorQuery).GetResults();
+
             if (deviceIndex > devices.Count)
             {
                 throw new ArgumentException($"Invalid Device Index.  Device Index of '{deviceIndex}' supplied.  '{devices.Count}' devices found.");
             }
-            var i2cSettings = new I2cConnectionSettings(slaveAddress)
-            {
-                BusSpeed = busSpeed,
-                SharingMode = sharingMode
-            };
-            connectionType = ConnectionType.I2C;
-            i2c = I2cDevice.FromIdAsync(devices[deviceIndex].Id, i2cSettings).GetResults();
-        }
 
-        public Wire(int deviceIndex, int chipSelectLine, int clockFrequency, int dataBitLength, SpiMode mode, SpiSharingMode sharingMode = SpiSharingMode.Exclusive)
-        {
-            var spiSelectorQuery = SpiDevice.GetDeviceSelector();
-            IReadOnlyList<DeviceInformation> devices = DeviceInformation.FindAllAsync(spiSelectorQuery).GetResults();
-            if (deviceIndex > devices.Count)
+            switch (this.connectionType)
             {
-                throw new ArgumentException($"Invalid Device Index.  Device Index of '{deviceIndex}' supplied.  '{devices.Count}' devices found.");
+                case ConnectionType.I2C:
+                    var i2cSettings = new I2cConnectionSettings(address)
+                    {
+                        BusSpeed = I2cBusSpeed.FastMode,
+                        SharingMode = I2cSharingMode.Exclusive
+                    };
+                    connectionType = ConnectionType.I2C;
+                    i2c = I2cDevice.FromIdAsync(devices[deviceIndex].Id, i2cSettings).GetResults();
+                    break;
+                case ConnectionType.SPI:
+                    var spiSettings = new SpiConnectionSettings(address)
+                    {
+                        ClockFrequency = 1000000,
+                        DataBitLength = 8,
+                        Mode = SpiMode.Mode0,
+                        SharingMode = SpiSharingMode.Exclusive
+                    };
+                    connectionType = ConnectionType.SPI;
+                    spi = SpiDevice.FromIdAsync(devices[deviceIndex].Id, spiSettings).GetResults();
+                    break;
             }
-            var spiSettings = new SpiConnectionSettings(chipSelectLine)
-            {
-                ClockFrequency = clockFrequency,
-                DataBitLength = dataBitLength,
-                Mode = mode,
-                SharingMode = sharingMode
-            };
-            connectionType = ConnectionType.SPI;
-            spi = SpiDevice.FromIdAsync(devices[deviceIndex].Id, spiSettings).GetResults();
         }
 
-        public void ReadData(byte[] buffer)
+        public void ReadData(byte[] buffer, uint readBytes)
+        {
+            if (buffer.Length < readBytes)
+            {
+                throw new ArgumentException($"Buffer length must be longer than bytes to read.  Buffer Length: {buffer.Length}  readBytes: {readBytes}");
+            }
+
+            var temp = new byte[readBytes];
+
+            switch(connectionType)
+            {
+                case ConnectionType.I2C:
+                    i2c.Read(temp);
+                    break;
+                case ConnectionType.SPI:
+                    spi.Read(temp);
+                    break;
+            }
+
+            for (int i = 0; i < readBytes; i++)
+            {
+                buffer[i] = temp[i];
+            }
+        }
+
+        public void WriteData(byte[] buffer)
         {
             switch(connectionType)
             {
                 case ConnectionType.I2C:
-                    i2c.Read(buffer);
+                    i2c.Write(buffer);
                     break;
                 case ConnectionType.SPI:
-                    spi.Read(buffer);
+                    spi.Write(buffer);
                     break;
             }
         }
